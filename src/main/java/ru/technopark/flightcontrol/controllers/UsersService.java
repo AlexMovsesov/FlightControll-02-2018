6f3ec6556ecba1afa0c4ac0fe5fdee0716daf89a
AlexMovsesov <alexmovsesov93@gmail.com>
2018-05-11 20:48:08 +0300

    cors to prod.

--- src/main/java/ru/technopark/flightcontrol/controllers/UsersService.java
+++ src/main/java/ru/technopark/flightcontrol/controllers/UsersService.java
@@ -20,7 +20,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 @CrossOrigin(origins = {
-        "*",
         "https://flightcontrolfrontend.herokuapp.com"
         },
         allowCredentials = "true",
